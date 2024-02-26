b54881b0c18dfb3a83b846be38551cfdec3c44e6
sadikmolla-01 <sadikmolla017.com>
2024-02-26 10:31:20 +0600

    increase or decrease numbers of phone in cart

---
+++ script.js
@@ -0,0 +1,67 @@
+// to add or remove I-phone's in a cart
+// to remove  product in cart
+
+document.getElementById('minus_product').addEventListener('click', function () {
+     let productNum = document.getElementById('productCount')
+     const ProductQuantity = parseInt(productNum.value)
+     const subtractProductQuantity = ProductQuantity - 1
+     productNum.value = subtractProductQuantity
+     let productAmount = document.getElementById('productAmount')
+     let totalProductAmount = subtractProductQuantity * 1219
+     productAmount.innerText = '$' + totalProductAmount
+})
+
+document.getElementById('add_product').addEventListener('click', function () {
+    handleProductChange()
+})
+function handleProductChange() {
+    let productNum = document.getElementById('productCount')
+    const ProductQuantity = parseInt(productNum.value)
+    const addProductQuantity = ProductQuantity + 1
+    productNum.value = addProductQuantity
+    let productAmount = document.getElementById('productAmount')
+    let totalProductAmount = addProductQuantity * 1219
+    productAmount.innerText = '$' + totalProductAmount
+}
+// document.getElementById('removePhoneCart').addEventListener('click', function () {
+//     let totalCart = document.getElementById('cartphone')
+//     totalCart.style.display = 'none'
+// })
+
+/**
+//1st step : call the minus button
+document.getElementById('minus_product').addEventListener('click', function () {
+    // 2nd step : call the product number field
+    let productNum = document.getElementById('productCount')
+    // 3rd step: access the product quantity field
+    const ProductQuantity = parseInt(productNum.value)
+    // 4th step: subtract the Product Quantity
+    const subtractProductQuantity = ProductQuantity - 1
+    // 5 th step: update the value
+    productNum.value = subtractProductQuantity
+    // 6. subtract amount
+    let productAmount = document.getElementById('productAmount')
+    let totalProductAmount = subtractProductQuantity * 1219
+    productAmount.innerText = '$' + totalProductAmount
+})
+
+// to add the product in cart
+document.getElementById('add_product').addEventListener('click', function () {
+    //  call the product number field
+    let productNum = document.getElementById('productCount')
+    // access the product quantity field
+    const ProductQuantity = parseInt(productNum.value)
+    // add the product quantity
+    const addProductQuantity  = ProductQuantity + 1
+    productNum.value = addProductQuantity
+    // add amount
+    let productAmount = document.getElementById('productAmount')
+    let totalProductAmount = addProductQuantity * 1219
+    productAmount.innerText = '$' + totalProductAmount
+})
+document.getElementById('removePhoneCart').addEventListener('click', function () {
+    let totalCart = document.getElementById('cartphone')
+    totalCart.style.display = 'none'
+})
+ */
+// to add or remove phone cover in the cart
